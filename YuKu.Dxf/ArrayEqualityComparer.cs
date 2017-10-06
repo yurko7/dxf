30acafcac35ba8e8ed5d25877b809277290fa0bd
Yuriy Kushnir <yurko7@hotmail.com>
2017-10-06 12:39:15 +0300

    Add DxfGroup.

---
+++ YuKu.Dxf/ArrayEqualityComparer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace YuKu.Dxf
+{
+    internal sealed class ArrayEqualityComparer<T> : IEqualityComparer<T[]>
+    {
+        public static ArrayEqualityComparer<T> Default => _default ?? (_default = new ArrayEqualityComparer<T>());
+
+        public Boolean Equals(T[] x, T[] y)
+        {
+            if (ReferenceEquals(x, y))
+            {
+                return true;
+            }
+
+            if (x == null || y == null)
+            {
+                return false;
+            }
+
+            if (x.Length != y.Length)
+            {
+                return false;
+            }
+
+            EqualityComparer<T> comparer = EqualityComparer<T>.Default;
+            for (Int32 i = 0; i < x.Length; ++i)
+            {
+                if (!comparer.Equals(x[i], y[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public Int32 GetHashCode(T[] obj)
+        {
+            return obj.GetHashCode();
+        }
+
+        private static ArrayEqualityComparer<T> _default;
+    }
+}
---
+++ YuKu.Dxf/DxfGroup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace YuKu.Dxf
+{
+    public struct DxfGroup : IEquatable<DxfGroup>
+    {
+        public static Boolean operator ==(DxfGroup left, DxfGroup right)
+        {
+            return left.Equals(right);
+        }
+
+        public static Boolean operator !=(DxfGroup left, DxfGroup right)
+        {
+            return !left.Equals(right);
+        }
+
+        public DxfGroup(Int16 code, Object value)
+        {
+            Code = code;
+            Value = value;
+        }
+
+        public Int16 Code { get; }
+
+        public Object Value { get; }
+
+        public override Boolean Equals(Object obj)
+        {
+            return obj is DxfGroup group && Equals(group);
+        }
+
+        public Boolean Equals(DxfGroup other)
+        {
+            Boolean equals = Code == other.Code;
+            if (equals)
+            {
+                equals = Value is Byte[] byteArray
+                    ? ArrayEqualityComparer<Byte>.Default.Equals(byteArray, other.Value as Byte[])
+                    : Equals(Value, other.Value);
+            }
+            return equals;
+        }
+
+        public override Int32 GetHashCode()
+        {
+            unchecked
+            {
+                Int32 valueHashCode = Value != null ? Value.GetHashCode() : 128;
+                return (Code.GetHashCode() * 397) ^ valueHashCode;
+            }
+        }
+    }
+}
